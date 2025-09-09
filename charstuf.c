513f0802b7a0c619915c1c35c82ea2acb4a50730
tris1412 <tdas_cse2305j6@mgit.ac.in>
2025-09-10 00:49:13 +0530

    Update charstuf.c

--- charstuf.c
+++ charstuf.c
@@ -1,42 +1,24 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-int main() {
-    char a[50], b[100];
-    char start[] = "dlestx";   
-    char end[]   = "dleetx";   
-    int n = 0, m = 0;
-
-    printf("Enter a string:\n");
-    scanf("%s", a);   
-
-    
-    strcpy(b, start);
-    m = strlen(start);
-
-    
-    while (a[n] != '\0') {
-        if (a[n] == 'd' && a[n+1] == 'l' && a[n+2] == 'e') {
-            
-            b[m++] = 'd';
-            b[m++] = 'l';
-            b[m++] = 'e';
-            b[m++] = 'd';
-            b[m++] = 'l';
-            b[m++] = 'e';
-            n += 3;  
-        } else {
-            b[m++] = a[n++];
-        }
-    }
-
-    
-    strcpy(&b[m], end);
-    m += strlen(end);
-
-    
-    b[m] = '\0';
-
-    printf("Stuffed Frame: %s\n", b);
-    return 0;
+void main()
+{
+char a[100];char b[100];
+int i,j=0;
+printf("enter a string:");
+scanf("%s",a);
+strcpy(b,"dlestx");
+j=strlen(b);
+for(i=0;a[i]!='\0';i++)
+{
+if(strncmp(&a[i],"dle",3)==0)
+{
+strcat(b+j,"dle");
+j+=3;
 }
+b[j++]=a[i];
+}
+strcat(b+j,"dleetx");
+printf("stuffed frame:%s",b);
+}
+
