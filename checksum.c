df011cda2a569d8884bf1692008a18d9559a7bcc
tris1412 <tdas_cse2305j6@mgit.ac.in>
2025-09-10 18:52:11 +0530

    Update checksum.c

--- checksum.c
+++ checksum.c
@@ -1,46 +1,40 @@
+#include<stdio.h>
+int onescompl(int num,int bits){
+return (~num) & ((1<<bits)-1);
+}
+void main()
 {
-    int n, bits;
-    printf("Enter the number of values: ");
-    scanf("%d", &n);
-
-    int data[n];
-    int sum = 0;
-
-    printf("Enter the numbers:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &data[i]);
-        sum += data[i];
-    }
-
-    printf("Enter the number of bits to represent the data: ");
-    scanf("%d", &bits);
-
-    // Calculate checksum
-    int checksum = onesComplement(sum, bits);
-
-    printf("\nAt sender's side:\n");
-    printf("Data: ");
-    for (int i = 0; i < n; i++) printf("%d ", data[i]);
-    printf("\nSum = %d", sum);
-    printf("\nChecksum = %d\n", checksum);
-
-    // At receiver
-    int newSum = sum + checksum;
-    int wrappedSum = newSum & ((1 << bits) - 1); // wrap around extra bits
-    int final = onesComplement(wrappedSum, bits);
-
-    printf("\nAt receiver's side:\n");
-    printf("Received Data: ");
-    for (int i = 0; i < n; i++) printf("%d ", data[i]);
-    printf("%d (checksum)\n", checksum);
-    printf("Sum of received data + checksum = %d\n", newSum);
-    printf("Wrapped sum (within %d bits) = %d\n", bits, wrappedSum);
-    printf("1's complement of wrapped sum = %d\n", final);
-
-    if (final == 0)
-        printf("No error detected.\n");
-    else
-        printf("Error detected.\n");
-
-    return 0;
+int n,bits;
+int sum=0;
+printf("enter no of values:");
+scanf("%d",&n);
+int data[n];
+printf("enter your data:\n");
+for(int i=0;i<n;i++)
+{
+scanf("%d",&data[i]);
+sum+=data[i];
+}
+printf("\n enter the no of bits to represent the dat");
+scanf("%d",&bits);
+int checksum=onescompl(sum,bits);
+printf("the senders side data:\n");
+for(int i=0;i<n;i++)
+{
+printf("%d",data[i]);
+}
+printf("the sum is %d\n",sum);
+printf("the checksum is %d\n",checksum);
+int recvsum=0;
+for(int i=0;i<n;i++)recvsum+=data[i];
+recvsum+=checksum;
+int wrapped=(recvsum) & ((1<<bits)-1);
+int final=onescompl(wrapped,bits);
+printf("the receive side:\n");
+for(int i=0;i<n;i++)printf("%d",data[i]);
+printf("%d(checksum)\n",checksum);
+printf("the sum of received+checksum:%d\n",recvsum);
+printf("wrapped(%d)bits=%d\n",bits,wrapped);
+printf("ones complement=%d",final);
+printf(final?"error detected\n":"no error detected\n");
 }
